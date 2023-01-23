e6944bdd6cba275fa6d76ef0f38452aa80f0d0f4
ArnurTanirbergenov <123318070+ArnurTanirbergenov@users.noreply.github.com>
2023-01-23 20:35:01 +0600

    Add files via upload

---
+++ tsis1 task2/create10.py
@@ -0,0 +1,7 @@
+mygpanow = 3.00
+mygpainfuture = 4.00
+txt = "{:.2f} is greater than {:.2f}"
+if mygpainfuture > mygpanow:
+    print(txt.format(mygpainfuture, mygpanow))
+    print(bool("Is it true"))
+    print(bool(0));
