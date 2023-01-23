9647018bc8e903f4a5f9ae53199fc6029901ae11
ArnurTanirbergenov <123318070+ArnurTanirbergenov@users.noreply.github.com>
2023-01-23 20:47:32 +0600

    Add files via upload

---
+++ tsis1 task2/create12.py
@@ -0,0 +1,18 @@
+Elamanheight = 180
+Andreyheight = 183
+Arnurheight = 190
+Igorseyeswidht = 0.00001
+if Elamanheight > Arnurheight:
+    print("Elaman is higher ")
+else:
+    print("Arnur is higher ")
+if Igorseyeswidht > 0.1:
+    print("Impossible")
+else:
+    print("We already knew it")
+if Andreyheight < Arnurheight and Elamanheight < Arnurheight:
+    print("Andrey and Elaman are gnoms")
+else:
+    print("Unreal")
+    #All names and numbers are unreal, without negative
+    
